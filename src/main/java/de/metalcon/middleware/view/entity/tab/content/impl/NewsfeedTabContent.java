1aaacfc19d68e6e50469b6a95e3db807e8634c52
Rene Pickhardt <rene@rene-pickhardt.de>
2014-03-11 15:58:01 +0100

    first tries in integrating the newsfeed to the spring middleware

--- src/main/java/de/metalcon/middleware/view/entity/tab/content/impl/NewsfeedTabContent.java
+++ src/main/java/de/metalcon/middleware/view/entity/tab/content/impl/NewsfeedTabContent.java
@@ -1,13 +1,24 @@
 package de.metalcon.middleware.view.entity.tab.content.impl;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import de.metalcon.middleware.backend.newsfeedserver.NewsFeedServer;
 import de.metalcon.middleware.view.entity.tab.EntityTabType;
 import de.metalcon.middleware.view.entity.tab.content.EntityTabContent;
 
 public class NewsfeedTabContent extends EntityTabContent {
+
+    @Autowired
+    private NewsFeedServer nfs;
 
     @Override
     public EntityTabType getEntityTabType() {
         return EntityTabType.NEWSFEED;
     }
 
+    public Map<String, Object> getNewsFeed() {
+        return nfs.getNewsFeed("3", "3", true);
+    }
 }
